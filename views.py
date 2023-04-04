0fee97a1b2eac6005955ae1107cc03b6249b292e
Amir Mohajerani <amohajerani6@gmail.com>
2023-04-03 22:47:25 -0400

    initial

---
+++ views.py
@@ -0,0 +1,16 @@
+from flask import Blueprint, render_template, request, jsonify
+
+views = Blueprint(__name__, 'views')
+
+
+@views.route('/')
+def home():
+    return render_template('index.html', name='king')
+
+
+@views.route('/profile/<username>')
+def profile(username):
+    args = request.args
+    name = args.get('name')
+    age = args.get('age')
+    return render_template('index.html', username=username, name=name, age=age)
