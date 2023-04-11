7dc2200ac2abbed4e0d2b3696fc26f671c9a6c8b
Amir Mohajerani <amohajerani6@gmail.com>
2023-04-11 00:33:40 -0400

    replace sql with mongodb

---
+++ app/config.py
@@ -0,0 +1,6 @@
+import os
+
+
+class Config(object):
+    SECRET_KEY = "some-secret-key"
+    MONGO_URI = "mongodb+srv://user1:College123College@cluster0.ahjf6ex.mongodb.net/?retryWrites=true&w=majority"
