920f6e2cf8ecc8f1c8a515dd5af5b203bc339d19
Amir Mohajerani <amohajerani6@gmail.com>
2023-04-22 16:42:37 -0400

    none

--- app/s3_functions.py
+++ app/s3_functions.py
@@ -9,11 +9,15 @@ if ENV_FILE:
     load_dotenv(ENV_FILE)
 
 
+def url_friendly(word):
+    return urllib.parse.quote_plus(word)
+
+
 def upload_file(file_name, bucket, user_id):
     s3_client = boto3.client('s3',
                              aws_access_key_id=os.environ.get('S3_KEY'),
                              aws_secret_access_key=os.environ.get('S3_SECRET'))
-    file_path = url = urllib.parse.quote_plus(user_id+'/'+file_name)
+    file_path = url_friendly(user_id)+'/'+file_name
     response = s3_client.upload_file(
         f"uploads/{file_name}",
         bucket,
@@ -28,11 +32,10 @@ def list_files(bucket, user_id):
                              aws_secret_access_key=os.environ.get('S3_SECRET'))
 
     public_urls = []
-    folder = user_id+'/'
+    folder = url_friendly(user_id)+'/'
     try:
         for item in s3_client.list_objects(Bucket=bucket, Prefix=folder)['Contents']:
             url = "https://"+bucket+".s3.amazonaws.com/"+folder+item['Key']
-            url = urllib.parse.quote_plus(url)
             filename = item['Key'].split('/')[-1]
             public_urls.append([url, filename])
     except Exception as e:
