4c71f706c82a7455813d89779a5f129547d9c0d8
Amir Mohajerani <amohajerani6@gmail.com>
2023-04-22 16:31:32 -0400

    none

--- app/s3_functions.py
+++ app/s3_functions.py
@@ -2,6 +2,7 @@ import boto3
 from dotenv import find_dotenv, load_dotenv
 import os
 import mimetypes
+import urllib
 
 ENV_FILE = find_dotenv()
 if ENV_FILE:
@@ -28,6 +29,7 @@ def list_files(bucket, user_id):
     try:
         for item in s3_client.list_objects(Bucket=bucket, Prefix=folder)['Contents']:
             url = "https://"+bucket+".s3.amazonaws.com/"+folder+item['Key']
+            url = urllib.parse.quote_plus(url)
             filename = item['Key'].split('/')[-1]
             public_urls.append([url, filename])
     except Exception as e:
