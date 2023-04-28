0e59b723575875eb2ed5533da25521aada455e77
Amir Mohajerani <amohajerani6@gmail.com>
2023-04-27 22:34:40 -0400

    none

--- run.sh
+++ run.sh
@@ -10,5 +10,5 @@ else
     sudo docker build -t nginx-image ./nginx
     sudo docker build -t app-image ./app
 fi
-sudo docker run --name nginx-container -d -p 80:80 -p 443:443 nginx-image
 sudo docker run --name app-container -d -p 8000:8000 -p 27017:27017 -p 27016:27016 -p 27015:27015 app-image
+sudo docker run --name nginx-container -d -p 80:80 -p 443:443 nginx-image
