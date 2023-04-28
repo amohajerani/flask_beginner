efcef6a8e6c9e3296a17bc5e7f4e6219392be102
Amir Mohajerani <amohajerani6@gmail.com>
2023-04-27 22:40:10 -0400

    none

--- run.sh
+++ run.sh
@@ -2,9 +2,12 @@
 
 
 sudo git pull
+sudo docker stop $(sudo docker ps -a -q)
+sudo docker rm $(sudo docker ps -a -q)
+sudo docker build -t app ./app
+sudo docker build -t nginx ./nginx
 
-sudo docker build -t app-image ./app
-sudo docker build -t nginx-image ./nginx
+sudo docker run --name app-container -d -p 8000:8000 -p 27017:27017 -p 27016:27016 -p 27015:27015 app
+sudo docker run --name nginx-container -d -p 80:80 -p 443:443 nginx
 
-sudo docker run --name app-container -d -p 8000:8000 -p 27017:27017 -p 27016:27016 -p 27015:27015 app-image
-sudo docker run --name nginx-container -d -p 80:80 -p 443:443 nginx-image
+sudo docker ps
