219a6304dc9283043bd98cda77ae31c7cba573a7
Amir Mohajerani <amohajerani6@gmail.com>
2023-04-27 22:37:13 -0400

    none

--- run.sh
+++ run.sh
@@ -2,13 +2,9 @@
 
 
 sudo git pull
-if [ "$1" == "nginx" ]; then
-    sudo docker build -t nginx-image ./nginx
-elif [ "$1" == "app" ]; then
-    sudo docker build -t app-image ./app
-else 
-    sudo docker build -t nginx-image ./nginx
-    sudo docker build -t app-image ./app
-fi
+
+sudo docker build -t app-image ./app
+sudo docker build -t nginx-image ./nginx
+
 sudo docker run --name app-container -d -p 8000:8000 -p 27017:27017 -p 27016:27016 -p 27015:27015 app-image
 sudo docker run --name nginx-container -d -p 80:80 -p 443:443 nginx-image
