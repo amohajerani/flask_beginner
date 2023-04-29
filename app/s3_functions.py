abdf4ec212bb7522d63a45d89e31fe3157737b1f
Amir Mohajerani <amohajerani6@gmail.com>
2023-04-28 23:25:05 -0400

    none

--- app/s3_functions.py
+++ app/s3_functions.py
@@ -18,14 +18,22 @@ def upload_file(file_location, bucket, username, filename):
                              aws_access_key_id=os.environ.get('S3_KEY'),
                              aws_secret_access_key=os.environ.get('S3_SECRET'))
     file_path = username+'/'+filename
+
+    '''  
+    if you decide to send files in chunks, use this code. You won't be able supply ExtraArgs and you don't need to.
+    For large files, we don't care about getting mimetype right because we want to download them.  
     config = boto3.s3.transfer.TransferConfig(multipart_threshold=1024 * 1024, max_concurrency=10,
                                               multipart_chunksize=1024 * 1024, use_threads=True)
     transfer = boto3.s3.transfer.S3Transfer(client=s3_client, config=config)
     response = transfer.upload_file(
         file_location,
         bucket,
-        file_path,
-        ExtraArgs={'ContentType': mimetypes.MimeTypes().guess_type(filename)[0]})
+        file_path)
+    '''
+    response = s3_client.upload_file(
+        file_location,
+        bucket,
+        file_path, ExtraArgs={'ContentType': mimetypes.MimeTypes().guess_type(filename)[0]})
     return response
 
 
